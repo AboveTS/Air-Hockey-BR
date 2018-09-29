61275a6f10db5397bc22928459601a1b14fb6194
ThomasTheDankEngine666 <sasha.dmytryshyn@gmail.com>
2018-09-29 17:14:34 -0600

    startscreen
    added sekelton of startscreen

---
+++ AirHockeySashaFork/Assets/Scripts/ButtonmanagerScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ButtonmanagerScript : MonoBehaviour {
+
+
+    public GameObject Settingbackground;
+    public void ButtonPressed(string one)
+    {
+        SceneManager.LoadScene("AfterStartScene", LoadSceneMode.Single);
+    }
+
+    public void ExitGameBtn(string two)
+    {
+        Application.Quit();
+    }
+
+    public void SettingBtn(string three)
+    {
+        Settingbackground.SetActive(true);
+    }
+
+    public void SettingBackBtn(string three)
+    {
+       Settingbackground.SetActive(false);
+    }
+
+
+}
